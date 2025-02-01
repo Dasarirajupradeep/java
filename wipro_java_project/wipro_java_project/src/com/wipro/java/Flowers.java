5a0c2947da9982994dbbfe58596f21de91501ec4
Dasarirajupradeep <pradeepdasariraju2002@gmail.com>
2025-02-01 17:33:28 +0530

    commit

---
+++ wipro_java_project/wipro_java_project/src/com/wipro/java/Flowers.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class Flowers {
+	String name;
+	String color;
+	
+
+	/**
+	 * 
+	 */
+	public Flowers(String fname,String fcolor)
+	
+	{
+		this.name=fname;
+		this.color=fcolor;
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+	public void display()
+	{
+		System.out.println("flower name"+name+"flower color"+fcolor)
+	}
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
