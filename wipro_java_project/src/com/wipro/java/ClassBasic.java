8d768a1ec4d065d6ca8e012e0f2b322bc633a80c
Dasarirajupradeep <pradeepdasariraju2002@gmail.com>
2025-02-02 11:45:48 +0530

    commit

---
+++ wipro_java_project/src/com/wipro/java/ClassBasic.java
@@ -0,0 +1,14 @@
+package com.wipro.java;
+
+public class ClassBasic {
+
+	public ClassBasic() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
